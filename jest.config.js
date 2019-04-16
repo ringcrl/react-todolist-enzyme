eaab88ebcbd3c4df594309a144d0279585530f38
Chenng <ringcrl@foxmail.com>
2019-04-16 17:43:46 +0800

    fix: fix error config

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   verbose: true,
   modulePaths: ['<rootDir>/__test__/'],
   moduleNameMapper: {
-    '.(css|less)$': '<rootDir>/__test__/NullModule.js'
+    '.(css|scss)$': '<rootDir>/__test__/NullModule.js'
   },
   setupFilesAfterEnv: ['<rootDir>/__test__/setupTests.js'],
   transform: {
